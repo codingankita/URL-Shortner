46144993b5d60a995381c6f3be03a9aeec896a4f
codingankita <ankita000bhatt@gmail.com>
2026-07-07 17:47:17 +0530

    Handling variables

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-    mongoose.connect('mongodb+srv://ankita000bhatt_db_user:r71Og2rwKTenKWWL@url-shortner.6myld4p.mongodb.net/?appName=URL-shortner')
+    await mongoose.connect(process.env.MONGO_URI);
 }
 
 module.exports = {
